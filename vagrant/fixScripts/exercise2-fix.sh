f3dcbf229611dec54815e78283bec5405e3fa0ae
c5271711 <dror.levin@sap.com>
2018-08-28 11:51:35 +0300

    Completed exercises 1-4

--- vagrant/fixScripts/exercise2-fix.sh
+++ vagrant/fixScripts/exercise2-fix.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 #add fix to exercise2 here
+grep -v www.ascii-art.de /etc/hosts > /tmp/hosts
+sudo mv /tmp/hosts /etc/hosts
--- vagrant/fixScripts/exercise3-fix.sh
+++ vagrant/fixScripts/exercise3-fix.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 #add fix to exercise3 here
+sed 's/deny from all/allow from all/' /etc/apache2/sites-available/default > /tmp/default
+sudo /bin/cp /tmp/default /etc/apache2/sites-available/default
+sudo service apache2 restart
