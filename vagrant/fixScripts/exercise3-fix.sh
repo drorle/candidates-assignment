169ff3ace2d7d9e34ad2f103fb0b4eb04468d2c0
c5271711 <dror.levin@sap.com>
2018-08-29 23:00:33 +0300

    Removed sudo

--- vagrant/fixScripts/exercise3-fix.sh
+++ vagrant/fixScripts/exercise3-fix.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #add fix to exercise3 here
-sed 's/deny from all/allow from all/' /etc/apache2/sites-available/default | sudo tee /etc/apache2/sites-available/default
-sudo service apache2 restart
+sed 's/deny from/allow from/' /etc/apache2/sites-available/default | sudo tee /etc/apache2/sites-available/default
+service apache2 restart
